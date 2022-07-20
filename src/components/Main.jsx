1f12223b62d45ac9f05ed59d0b9f4cd94a1fbc05
Daniel Kathiresan <daniel.kathiresan@gmail.com>
2022-07-20 21:14:16 +1200

    Adding custom text styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,16 @@
-import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop: Constants.statusBarHeight,
-        flexGrow: 1,
-        flexShrink: 1,
-    },
-});
+import Text from './Text';
 
 const Main = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Rate Repository Application</Text>
-        </View>
-    );
+  return (
+    <>
+      <Text>Simple text</Text>
+      <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
+      <Text fontWeight="bold" fontSize="subheading">
+        Bold subheading
+      </Text>
+      <Text color="textSecondary">Text with secondary color</Text>
+    </>
+  );
 };
 
 export default Main;
